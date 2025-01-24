d3db97a1001e363b37038084e15fed557e809d5e
Amitkumarsharma7082 <aksharma7404@gmail.com>
2025-01-24 10:33:26 +0530

    Pattern 11 One&Zero

---
+++ LogicBuilding/Pattern11.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    /*
+    1
+    01
+    101
+    0101
+    10101
+    */
+   int n;
+   cout << "Enter the value of n : " << endl;
+   cin >> n;
+
+   // Outer Loop
+   for(int i = 1; i <= n; i++) {
+    // Inner Loop
+    for(int j = 1; j <= i; j++) {
+        // Print
+    }
+   }
+}
