40b594f9cdbb688b54f48f415a9c4ea89d7a6684
Amitkumarsharma7082 <aksharma7404@gmail.com>
2025-01-29 20:15:07 +0530

    Pair C++ STL

---
+++ 4 STL C++/2_Pair.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void explainPair() {
+    // pair<int, int> pt1 = {2, 8};
+    pair<int, int> pt1(10, 20);
+    cout << "Pair 1 : " << pt1.first << " and Pair 2 : " << pt1.second << endl;
+}
+
+int main() {
+    explainPair();
+}
+
