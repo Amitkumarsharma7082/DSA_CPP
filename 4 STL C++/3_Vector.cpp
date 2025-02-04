98a6fb4e055672dbe3fb90a003eb2739f4328bc5
Amitkumarsharma7082 <aksharma7404@gmail.com>
2025-02-04 18:01:35 +0530

    vector with auto

--- 4 STL C++/3_Vector.cpp
+++ 4 STL C++/3_Vector.cpp
@@ -12,19 +12,29 @@ void explainVector() {
     }
     cout << endl;
 
-    // vector<int> :: iterator beginItr = vec.begin();
-    // vector<int> :: iterator endItr = vec.end();
-
-    // for (vector<int> :: iterator i = beginItr; i < endItr; i++) {
-    //     cout << *i << " ";
-    // }
-    // cout << endl;
-
     auto beginItr = vec.begin();
     auto endItr = vec.end();
 
     for (auto i = beginItr; i < endItr; i++) {
         cout << *i << " ";
+    }
+    cout << endl;
+
+    vector<int> v;
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);
+    
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+
+    auto vBeginItr = v.begin();
+    auto vEndItr = v.end();
+
+    for (auto j = vBeginItr; j < vEndItr; j++) {
+        cout << *j << " ";
     }
     cout << endl;
 }
@@ -33,3 +43,27 @@ int main() {
     explainVector();
     return 0;
 }
+
+
+/*
+ Notes : 
+
+(1) 
+  vector<int> :: iterator beginItr = vec.begin();
+    vector<int> :: iterator endItr = vec.end();
+
+    for (vector<int> :: iterator i = beginItr; i < endItr; i++) {
+        cout << *i << " ";
+    }
+    cout << endl;
+
+
+(2)    
+    vector<int> :: iterator beginItr = vec.begin();
+    auto > vector<int> :: iterator
+    beginItr > name of the vector
+    vec > vector<int> vec
+    begin start from vec[0] element
+    end last element vec[n-1] element
+
+*/
