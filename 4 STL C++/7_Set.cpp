8c9cca990880ddcf93168726a000ffff75f10b1b
Amitkumarsharma7082 <aksharma7404@gmail.com>
2025-02-10 14:44:37 +0530

    set with l_bound & u_bound

--- 4 STL C++/7_Set.cpp
+++ 4 STL C++/7_Set.cpp
@@ -18,8 +18,61 @@ void explainSet() {
      }
      cout << endl;
 
+/*
+    lower_bound :
+        returns an iterator that points to an element
+        that is >= number given
 
-     // erase mid element : it1, it2
+        example 1 : 11 this lower_bound check/point to just greater or equal
+        number in the stack answer is 12
+
+        example 2 : if we give 214 (this element is not in stack)
+        so it is point after the stack like end;
+*/ 
+
+    // Example 1 : lower_bound
+    auto it = st.lower_bound(11) ; 
+    cout << *it << " ";
+    cout << endl;
+
+    // Example 2 : lower_bound (element not in stack)
+    auto it2 = st.lower_bound(214);
+    if(it2 == st.end()) {
+        cout << "end" << " " << endl;
+    }
+     
+/*
+
+    Upper_bound :
+    that points to > to element
+
+*/
+
+//  Example : upper_bound (11) return (12) and 
+// (12) return (21) 
+
+    auto it3 = st.upper_bound(213) ; // point to end iterator
+    cout << "it3 : "<<*it << endl;
+    if (it3 == st.end()) {
+        cout << "end" << " ";
+    }
+    cout << endl;
+
+}
+
+int main() {
+    explainSet();
+
+}
+
+
+
+
+/*
+
+Notes :
+
+// erase mid element : it1, it2
 
 // this method is for : st.begin() + 1 [go to 0,1,2,3,4 : 1 element]
     auto it1 = st.begin();
@@ -38,21 +91,7 @@ void explainSet() {
     }
     cout << endl;
 
-    
 
-}
-
-int main() {
-    explainSet();
-
-}
-
-
-
-
-/*
-
-Notes :
   while (st.empty() == false) {
         cout << st.top() << " ";
         st.pop();
