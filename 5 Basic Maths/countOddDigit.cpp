a456c683b4cf5b7bee8a7bf32881313333acde06
Amitkumarsharma7082 <aksharma7404@gmail.com>
2025-02-11 14:48:34 +0530

    formatting

--- 5 Basic Maths/countOddDigit.cpp
+++ 5 Basic Maths/countOddDigit.cpp
@@ -2,22 +2,23 @@
 using namespace std;
 
 int countOddDigit(int n) {
-// Base case
-    if (n == 0) {
-        return 1;
-    }
-// CountOddDigit : n%10 : lastDigit to check 
-int count = 0;
-    while (n > 0) {
-        int lastDigit = n % 10;
-
-        // it is odd
-        if (lastDigit % 2 == 1) {
-            count = count + 1;
+        // Base case
+        if (n == 0) {
+            return 1;
         }
-        n = n / 10;
+        // CountOddDigit : n%10 : lastDigit to check
+        int count = 0;
+        while (n > 0) {
+            int lastDigit = n % 10;
+
+            // it is odd
+            if (lastDigit % 2 == 1) {
+                count = count + 1;
+            }
+            n = n / 10;
+        }
+        return count;
     }
-    return count;
 }
 
 int main() {
