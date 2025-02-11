f40fc448d88d5f125e6982cc08434cb155c304a5
Amitkumarsharma7082 <aksharma7404@gmail.com>
2025-02-11 14:47:10 +0530

    Solution countOddDigit

---
+++ 5 Basic Maths/countOddDigit.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int countOddDigit(int n) {
+// Base case
+    if (n == 0) {
+        return 1;
+    }
+// CountOddDigit : n%10 : lastDigit to check 
+int count = 0;
+    while (n > 0) {
+        int lastDigit = n % 10;
+
+        // it is odd
+        if (lastDigit % 2 == 1) {
+            count = count + 1;
+        }
+        n = n / 10;
+    }
+    return count;
+}
+
+int main() {
+    int n;
+    cout << "Enter the number : ";
+    cin >> n;
+
+    int count = countOddDigit(n);
+    cout << "count : " << count << endl;
+}
