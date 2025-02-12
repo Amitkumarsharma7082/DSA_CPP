b789330b10606eb0b98265c83131bd550fce73ef
Amitkumarsharma7082 <aksharma7404@gmail.com>
2025-02-13 01:08:21 +0530

    LCM function add

--- 5 Basic Maths/11_GCD.cpp
+++ 5 Basic Maths/11_GCD.cpp
@@ -11,6 +11,25 @@ int GCD(int n1, int n2) {
     }
     return num;
 }
+/*
+ LCM :
+ int lcm;
+        int n = max(n1, n2);
+        int i =1;
+
+        while (1) {
+            int mul = n* i;
+
+            if(mul % n1 == 0 && mul % n2 == 0) {
+                lcm = mul;
+                break;
+            }
+            i++;
+        }
+
+        return lcm;
+    }
+*/
 
 int main() {
     int n1, n2;
