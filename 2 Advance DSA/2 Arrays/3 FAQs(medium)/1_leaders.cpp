4b8841becd841c8ae07f1781b385c66e074cbadf
Amitkumarsharma7082 <aksharma7404@gmail.com>
2025-04-24 18:54:22 +0530

    Brute solution : leaders in array

--- 2 Advance DSA/2 Arrays/3 FAQs(medium)/1_leaders.cpp
+++ 2 Advance DSA/2 Arrays/3 FAQs(medium)/1_leaders.cpp
@@ -1,50 +1,45 @@
+//! Brute Force Solution : 
 #include<bits/stdc++.h>
 using namespace std;
 
 class Solution {
- public : 
- vector<int> leaders(vector<int>& arr) {
-    int n = arr.size();
-    vector<int> leadArr;
-    for (int i = 0; i < n; i++) {
-        int leader = true; 
-        for (int j = i+1; j < n; j ++) {
-            if (arr[j] > arr[i]) {
-                leader = false;
-                break;
+    public :
+    vector<int> leaders(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> ans;
+        for (int i = 0; i < n; i++) {
+            bool leader = true;
+            for (int j = i+1; j < n; j++) {
+                if(arr[j] > arr[i]) {
+                    leader = false;
+                    break;
+                }
+            }
+            if (leader == true) {
+                ans.push_back(arr[i]);
             }
         }
-        if (leader == true) {
-            leadArr.push_back(arr[i]);
-        }
+        // Return the leaders
+        return ans;
     }
-    return leadArr;
- }
 };
-
 int main() {
-    vector<int> arr;
-    
+    vector<int>arr;
     int n, val;
-    cout << "Enter the value of n : ";
+    cout << "Enter the value of n :";
     cin >> n;
-    cout << "Enter an array : ";
+    cout << "Enter the array : ";
     for (int i = 0; i < n; i++) {
         cin >> val;
         arr.push_back(val);
     }
-    
-
     Solution finder;
-
-    vector<int> inter = finder.leaders(arr);
-    if (inter.empty()) {
-        cout << "No elements found.";
-    }
-    for (int i = 0; i < inter.size(); i++) {
-        cout << inter[i] << " ";
+    vector<int> newArr = finder.leaders(arr);
+    cout << "Leaders in the array are : ";
+    for (int i = 0; i < newArr.size(); i++) {
+        cout << newArr[i] << " ";
     }
     cout << endl;
-
     return 0;
+
 }
