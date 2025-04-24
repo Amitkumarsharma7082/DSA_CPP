5fdff48c456b6b464f521ede5106ef921883a45b
Amitkumarsharma7082 <aksharma7404@gmail.com>
2025-04-24 19:23:12 +0530

    Optimal Solution : leaders array

--- 2 Advance DSA/2 Arrays/3 FAQs(medium)/1_leaders.cpp
+++ 2 Advance DSA/2 Arrays/3 FAQs(medium)/1_leaders.cpp
@@ -1,28 +1,48 @@
-//! Brute Force Solution : 
 #include<bits/stdc++.h>
 using namespace std;
 
+//! Brute Force Solution : T.C. -> O(N^2) & S.C. -> O(N)
+// class Solution {
+//     public :
+//     vector<int> leaders(vector<int>& arr) {
+//         int n = arr.size();
+//         vector<int> ans;
+//         for (int i = 0; i < n; i++) {
+//             bool leader = true;
+//             for (int j = i+1; j < n; j++) {
+//                 if(arr[j] > arr[i]) {
+//                     leader = false;
+//                     break;
+//                 }
+//             }
+//             if (leader == true) {
+//                 ans.push_back(arr[i]);
+//             }
+//         }
+//         // Return the leaders
+//         return ans;
+//     }
+// };
+
+//! Optimal Solution : T.C. -> O(N) + O(NlogN) & S.C. -> O(N)
 class Solution {
-    public :
+    public:
     vector<int> leaders(vector<int>& arr) {
-        int n = arr.size();
         vector<int> ans;
-        for (int i = 0; i < n; i++) {
-            bool leader = true;
-            for (int j = i+1; j < n; j++) {
-                if(arr[j] > arr[i]) {
-                    leader = false;
-                    break;
-                }
-            }
-            if (leader == true) {
+        int n = arr.size();
+        int maxi = INT_MIN;
+        for (int i = n - 1; i >= 0; i--) {
+            if(arr[i] > maxi) {
                 ans.push_back(arr[i]);
             }
+            maxi = max(maxi, arr[i]);
         }
-        // Return the leaders
+        // reverse of the array : 
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
+
 int main() {
     vector<int>arr;
     int n, val;
