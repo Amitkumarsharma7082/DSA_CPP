8972945f0fa83b703e7d3f52992a1614738c9134
Amitkumarsharma7082 <aksharma7404@gmail.com>
2025-04-28 14:10:32 +0530

    changes

--- 2 Advance DSA/2 Arrays/3 FAQs(medium)/1_leaders.cpp
+++ 2 Advance DSA/2 Arrays/3 FAQs(medium)/1_leaders.cpp
@@ -33,7 +33,7 @@ class Solution {
         int maxi = INT_MIN;
         for (int i = n - 1; i >= 0; i--) {
             if(arr[i] > maxi) {
-                ans.push_back(arr[i]);
+                ans.push_back(arr[i]); 
             }
             maxi = max(maxi, arr[i]);
         }
